41f86f5994c931c40b799c7ff749ec9cf65dfa8d
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-03-19 12:43:04 +0100

    add comics array

---
+++ src/Data/DataComics.js
@@ -0,0 +1,9 @@
+const DataComics = [
+  {
+    titolo: "Viaggio Interstellare",
+    cover: "/assets/img/viaggioIntStellar/cover.jpg",
+
+    anno: "2021",
+  },
+];
+export default DataComics;
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,68 +4,35 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "./HomePage.css";
 import "swiper/css";
 import BookIcons from "../../assets/icons/book.png";
+import DataComics from "../../Data/DataComics";
 
 const Homepage = () => {
   return (
     <div className="background-home">
       <div className="card-home">
         <Swiper className="mySwiper">
-          <SwiperSlide className="slide-1">
-            <div className="slide-header">
-              <div className="title-header">
-                <h2>Viaggio Interstellare</h2>
+          {DataComics.map((element, index) => (
+            <SwiperSlide
+              className="slide-1"
+              style={{ backgroundImage: `url(${element.cover})` }}
+            >
+              <div className="slide-header">
+                <div className="title-header">
+                  <h2>{element.titolo}</h2>
+                </div>
+                <div className="ancor-header">
+                  <p>Storyline</p>
+                  <p>Cast</p>
+                </div>
               </div>
-              <div className="ancor-header">
-                <p>Storyline</p>
-                <p>Cast</p>
+              <div className="slide-body">
+                <Button variant="outline-primary">
+                  <img src={BookIcons} alt="Book Icon" className="book-icon" />
+                  Read
+                </Button>
               </div>
-            </div>
-            <div className="slide-body">
-              <Button variant="outline-primary">
-                <img src={BookIcons} alt="Book Icon" className="book-icon" />
-                Read
-              </Button>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <div className="slide-header">
-              <div className="title-header">Nome fumetto</div>
-              <div className="ancor-header">
-                <p>Storyline</p>
-                <p>Cast</p>
-              </div>
-            </div>
-            <div className="slide-body">
-              <Button variant="outline-primary">Primary</Button>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <div className="slide-header">
-              <div className="title-header">Nome fumetto</div>
-              <div className="ancor-header">
-                <p>Storyline</p>
-                <p>Cast</p>
-              </div>
-            </div>
-            <div className="slide-body">
-              <Button variant="outline-primary">Primary</Button>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <div className="slide-header">
-              <div className="title-header">Nome fumetto</div>
-              <div className="ancor-header">
-                <p>Storyline</p>
-                <p>Cast</p>
-              </div>
-            </div>
-            <div className="slide-body">
-              <Button variant="outline-primary">Primary</Button>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
