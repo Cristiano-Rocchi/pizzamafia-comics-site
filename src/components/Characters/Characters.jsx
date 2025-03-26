965a1bc4e3e2d10a63831a0e058347f487e6488b
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-03-26 23:06:19 +0100

    update style characters page

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -4,6 +4,8 @@ import { Col, Container, Row } from "react-bootstrap";
 import DataComics from "../../Data/DataComics";
 import { useNavigate } from "react-router-dom";
 
+import hunderline from "../../assets/icons/hunderline.svg";
+
 const Characters = () => {
   // HOOKS
   const [selectedCharacter, setSelectedCharacter] = useState(null);
@@ -11,17 +13,16 @@ const Characters = () => {
   const [comicTitle, setComicTitle] = useState("");
   const [comicId, setComicId] = useState("");
 
-  const navigate = useNavigate(); // ✅ Per navigare tra le pagine
+  const navigate = useNavigate();
 
   // FUNZIONI
   const handleCharacterClick = (character, comic) => {
     setSelectedCharacter(character);
     setShowDetails(true);
     setComicTitle(comic.titolo);
-    setComicId(comic.id); // ✅ Salva l'id del fumetto per il link
+    setComicId(comic.id);
   };
 
-  // Naviga alla pagina del fumetto
   const goToComicPage = () => {
     navigate(`/comics/${comicId}`);
   };
@@ -33,11 +34,19 @@ const Characters = () => {
         <Col xs={4} className={`details-panel ${showDetails ? "visible" : ""}`}>
           {selectedCharacter && (
             <div className="card-characters-details">
-              <h2>{selectedCharacter.nome}</h2>
+              <h2>· {selectedCharacter.nome} ·</h2>
               <img src={selectedCharacter.foto} alt={selectedCharacter.nome} />
-              <p className="text-start mt-3">{selectedCharacter.descrizione}</p>
-              <p>
-                Fumetto: <span onClick={goToComicPage}>{comicTitle}</span>
+              <p className="text-start mt-3">
+                "{selectedCharacter.descrizione}"
+              </p>
+              <p className="d-flex justify-content-end gap-2">
+                Fumetto:
+                <div
+                  className="footer-characters-details"
+                  onClick={goToComicPage}
+                >
+                  <p className="mb-0">{comicTitle}</p>
+                </div>
               </p>
             </div>
           )}
@@ -45,7 +54,10 @@ const Characters = () => {
 
         {/* CARD PERSONAGGI */}
         <Col className="mt-5" xs={7}>
-          <h1 className="text-center">Tutti i Personaggi</h1>
+          <div className="title-characters">
+            <h1>· Tutti i Personaggi ·</h1>
+            <img src={hunderline} alt="" />
+          </div>
           <div className="card-characters-container">
             {DataComics.map((comic) =>
               comic.cast.map((element, index) => (
