0846e543a67b23148d95673c15a9224d1ae6f182
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-03-28 18:49:23 +0100

    add drawing array data

---
+++ src/Data/DataDrawing.js
@@ -0,0 +1,38 @@
+const DataDrawing = [
+  {
+    id: "one-more-anarchist",
+    titolo: "one more anarchist",
+    img: "",
+    anno: 2018,
+    produzione: "Uniposca su Carta",
+  },
+  {
+    id: "wor(l)d-error",
+    titolo: "Worl(d) Error",
+    img: "",
+    anno: 2020,
+    produzione: "Digitale",
+  },
+  {
+    id: "a-ticket-for-hell",
+    titolo: "A ticket for Hell",
+    img: "",
+    anno: 2017,
+    produzione: "Digitale",
+  },
+  {
+    id: "buon-appetito",
+    titolo: "Buon Appetito",
+    img: "",
+    anno: 2015,
+    produzione: "Digitale",
+  },
+];
+
+export default DataDrawing;
+
+/* id: 
+titolo: 
+img:
+anno:
+produzione: */
