cf723a8c4933f8c30f9d154128d1a7c3dcff9267
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-03-29 18:35:35 +0100

    update style homepage

---
+++ src/Data/DataDrawings.js
@@ -0,0 +1,53 @@
+import Disegno1 from "../assets/img/drawings/disegno1.jpg";
+import Disegno2 from "../assets/img/drawings/disegno 2.png";
+import Disegno2_1 from "../assets/img/drawings/disegno2.1.jpg";
+import Disegno3 from "../assets/img/drawings/disegno3.jpg";
+import Disegno4 from "../assets/img/drawings/disegno4.png";
+import Disegno4_1 from "../assets/img/drawings/disegno4.1.png";
+import Graffito from "../assets/img/drawings/graffito2.jpg";
+
+const DataDrawings = [
+  {
+    id: "one-more-anarchist",
+    titolo: "one more anarchist",
+    img: Disegno1,
+    anno: 2018,
+    produzione: "Uniposca su Carta",
+  },
+  {
+    id: "wor(l)d-error",
+    titolo: "Worl(d) Error",
+    img: Disegno2,
+    anno: 2020,
+    produzione: "Digitale",
+  },
+  {
+    id: "a-ticket-for-hell",
+    titolo: "A ticket for Hell",
+    img: Disegno3,
+    anno: 2017,
+    produzione: "Digitale",
+  },
+  {
+    id: "buon-appetito",
+    titolo: "Buon Appetito",
+    img: Disegno4,
+    anno: 2015,
+    produzione: "Digitale",
+  },
+  {
+    id: "graffito",
+    titolo: "Graffito",
+    img: Graffito,
+    anno: 2019,
+    produzione: "Digitale",
+  },
+];
+
+export default DataDrawings;
+
+/* id: 
+titolo: 
+img:
+anno:
+produzione: */
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,9 +3,10 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import "./HomePage.css";
 import BookIcons from "../../assets/icons/book.png";
 import DataComics from "../../Data/DataComics";
+import DataDrawings from "../../Data/DataDrawings";
 import { useNavigate } from "react-router-dom";
 import imgPizzamafia from "../../assets/img/bg-home/pizzamafia.png";
-import cloud from "../../assets/img/bg-home/ballonpng.png";
+
 //librerie
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -19,11 +20,18 @@ const Homepage = () => {
   //Hook
   const navigate = useNavigate();
   const [swiperInstance, setSwiperInstance] = useState(null);
+  const [swiperInstance2, setSwiperInstance2] = useState(null);
 
   //FUNZIONI
   const handleComicClick = (index) => {
     if (swiperInstance) {
       swiperInstance.slideTo(index);
+    }
+  };
+
+  const handleDrawingClick = (index) => {
+    if (swiperInstance2) {
+      swiperInstance2.slideTo(index);
     }
   };
   //MODALI CAST E STORYLINE
@@ -51,6 +59,8 @@ const Homepage = () => {
   };
   return (
     <>
+      {/* --------------COMICS------------ */}
+
       <Container className="home-container">
         <Row className="sect-intro">
           <Col xs={4}>
@@ -68,7 +78,6 @@ const Homepage = () => {
         </Row>
         <Col className="sect-title" xs={12}>
           <h1>Fumetti</h1>
-          <h1>Comics</h1>
         </Col>
 
         <Row className="sect-works d-flex justify-content-end">
@@ -173,7 +182,6 @@ const Homepage = () => {
               </div>
             </div>
           </Col>
-          <Col xs={12}></Col>
         </Row>
       </Container>
     </>
@@ -182,97 +190,93 @@ const Homepage = () => {
 
 export default Homepage;
 
-/*  return (
-    <>
-      <div className="background-home d-flex">
-        <section className="section-intro">
-          <img className="img-pizzamafia" src={imgPizzamafia} alt="" />
-          <div>
-            <img className="img-cloud" src={cloud} alt="" />
+/* 
 
-            <h1>aa</h1>
-          </div>
-        </section>
+ {/
+ <Row className="sect-intro mt-5 mb-5">
+ <Col className="sect-title2" xs={12}>
+   <h1>Disegni</h1>
+ </Col>
+</Row>
+<Row className="sect-works2 d-flex justify-content-end">
+ <Col xs={12}>
+   <div className="d-flex justify-content-between">
+     <div className="drawings-home-container">
+       <h2>tutti i fumetti</h2>
+       <div className="all-drawings">
+         {DataDrawings.map((element, index) => (
+           <div
+             key={index}
+             onClick={() => handleDrawingClick(index)}
+             className="card-drawing"
+           >
+             <img src={element.img} alt={element.titolo} />
+             <h5>{element.titolo}</h5>
+           </div>
+         ))}
+       </div>
+     </div>
+     <div className="drawings-slide-home">
+       <motion.div
+         className="card-home"
+         initial={{ y: -200, opacity: 0 }}
+         animate={{ y: 0, opacity: 1 }}
+         transition={{
+           type: "spring",
+           stiffness: 150,
+           damping: 10,
+         }}
+       >
+         <Swiper
+           className="swiper-home"
+           rewind={true}
+           navigation={true}
+           modules={[Navigation]}
+           onSwiper={(swiper) => setSwiperInstance2(swiper)}
+           onInit={(swiper) => {
+             const nextButton = swiper.navigation.nextEl;
+             const prevButton = swiper.navigation.prevEl;
 
-        <motion.div
-          className="card-home"
-          initial={{ y: -200, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            type: "spring",
-            stiffness: 150,
-            damping: 10,
-          }}
-        >
-          <Swiper
-            className="swiper-home"
-            rewind={true}
-            navigation={true}
-            modules={[Navigation]}
-            onInit={(swiper) => {
-              const nextButton = swiper.navigation.nextEl;
-              const prevButton = swiper.navigation.prevEl;
-
-              // Rimuovi le icone predefinite
-              nextButton.innerHTML = ">";
-              prevButton.innerHTML = "<";
-            }}
-          >
-            {DataComics.map((element, index) => (
-              <SwiperSlide
-                key={index}
-                className="slide-1"
-                style={{ backgroundImage: `url(${element.cover})` }}
-              >
-                <div className="slide-header">
-                  <div className="title-header">
-                    <h2>{element.titolo}</h2>
-                  </div>
-                  <div className="ancor-header">
-                    <Tippy
-                      className="tippy-story"
-                      content={element.storyline}
-                      placement="bottom"
-                      delay={100}
-                      interactive={true}
-                      animation="bounce"
-                    >
-                      <h5>Storyline</h5>
-                    </Tippy>
-                    <Tippy
-                      className="tippy-cast"
-                      content={renderCastContent(element.cast)}
-                      placement="bottom"
-                      delay={100}
-                      interactive={true}
-                      animation="bounce"
-                    >
-                      <h5>Cast</h5>
-                    </Tippy>
-                  </div>
-                </div>
-                <div className="slide-body">
-                  <Button
-                    variant="outline-primary"
-                    onClick={() => navigate(`/comics/${element.id}`)}
-                  >
-                    <img
-                      src={BookIcons}
-                      alt="Book Icon"
-                      className="book-icon"
-                    />
-                    Read
-                  </Button>
-                </div>
-                <div className="slide-footer">
-                  <p className="comics-info">
-                    {element.anno} | {element.produzione}
-                  </p>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </motion.div>
-      </div>
-    </>
-  );*/
+             // Rimuovi le icone predefinite
+             nextButton.innerHTML = ">";
+             prevButton.innerHTML = "<";
+           }}
+         >
+           {DataDrawings.map((element, index) => (
+             <SwiperSlide
+               key={index}
+               className="slide-1"
+               style={{ backgroundImage: `url(${element.img})` }}
+             >
+               <div className="slide-header">
+                 <div className="title-header">
+                   <h2>{element.titolo}</h2>
+                 </div>
+               </div>
+               <div className="slide-body">
+                 <Button
+                   variant="outline-primary"
+                   onClick={() => navigate(`/comics/${element.id}`)}
+                 >
+                   <img
+                     src={BookIcons}
+                     alt="Book Icon"
+                     className="book-icon"
+                   />
+                   Vedi
+                 </Button>
+               </div>
+               <div className="slide-footer">
+                 <p className="comics-info">
+                   {element.anno} | {element.produzione}
+                 </p>
+               </div>
+             </SwiperSlide>
+           ))}
+         </Swiper>
+       </motion.div>
+     </div>
+   </div>
+ </Col>
+</Row>
+*/
